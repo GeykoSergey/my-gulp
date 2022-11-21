b8bc5f19cbcd0d031ad26cf39d5b3a448f98dc8c
Geyko Sergey <sergeyko.dnepr@gmail.com>
2022-11-21 22:31:58 +0200

    add file

--- src/js/app.js
+++ src/js/app.js
@@ -25,12 +25,16 @@ import * as flsFunctions from "./files/functions.js";
 /* Проверка поддержки webp, добавление класса webp или no-webp для HTML */
 /* (i) необходимо для корректного отображения webp из css  */
 flsFunctions.isWebp();
+
 /* Добавление класса touch для HTML если браузер мобильный */
 // flsFunctions.addTouchClass();
+
 /* Добавление loaded для HTML после полной загрузки страницы */
 // flsFunctions.addLoadedClass();
+
 /* Модуль для работы с меню (Бургер) */
 // flsFunctions.menuInit();
+
 /* Учет плавающей панели на мобильных устройствах при 100vh */
 // flsFunctions.fullVHfix();
 
@@ -46,7 +50,7 @@ flsFunctions.isWebp();
 Документация: https://template.fls.guru/template-docs/modul-taby.html
 Сниппет (HTML): tabs
 */
-// flsFunctions.tabs();
+flsFunctions.tabs();
 
 /*
 Модуль "показать еще"
